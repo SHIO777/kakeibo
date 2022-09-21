6c787e2c4e7f618d3bd34b6df84259fef9378c02
SHIO777 <shiotatsu1015@gmail.com>
2022-09-21 23:20:16 +0900

    [add] categories and transactions table

--- database/migrations/2022_09_21_141753_create_categories_table.php
+++ database/migrations/2022_09_21_141753_create_categories_table.php
@@ -13,9 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('transactions', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            // $table->string()
+            $table->string('category');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('transactions');
+        Schema::dropIfExists('categories');
     }
 };
