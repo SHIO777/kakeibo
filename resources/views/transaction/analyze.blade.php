07254e7e6f0f466d34d2cd17f65f6faf4f34f0d4
SHIO777 <shiotatsu1015@gmail.com>
2022-09-26 10:09:14 +0900

    [edit] analyze page -> responsive

--- resources/views/transaction/analyze.blade.php
+++ resources/views/transaction/analyze.blade.php
@@ -9,6 +9,10 @@
     <script src="{{ mix('/js/chartjs.js') }}"></script>
     <script src="{{ mix('/js/app.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.js"></script>
+    {{-- 日本語の日付を表示するには以下が必要 --}}
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/locale/ja.js"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-moment@1.0.0"></script>
     <script type="text/javascript">
         const ctx = document.getElementById("myChart").getContext("2d");
         const myChart = new Chart(ctx, {
@@ -32,15 +36,25 @@
                         time: {
                           tooltipFormat: 'DD T'
                         },
-                        display: true,
+                        title: {
+                            display: true,
+                            text: 'Date'
+                        }
+                    },
+                    y: {
+                        title: {
+                            display: true,
+                            text: 'value'
+                        }
                     }
-                }
+                }, 
+                responsive: true,
+                maintainAspectRatio: false,      // レスポンシブ対応のため
             }
         })
     
-        $("#getdata").click(function(e) {
-            console.log('push');
-            e.preventDefault();
+
+        function getData() {
             $.ajax({
                 type: "get",
                 // url: "/getdata",
@@ -69,23 +83,41 @@
             .fail((error) => {
                 console.log(error.statusText);
             });
+        };
+
+        // 初期実行
+        getData();
+
+        // getdataボタンが押されたら実行
+        $("#getdata").click(function(e) {
+            console.log('push');
+            e.preventDefault();
+            getData();
         });
     </script>
   </x-slot>
 
-  {{-- <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:w-8/12 md:w-1/2 lg:w-5/12">
-      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
+<div class="py-12">
+    {{-- <div class="max-w-7xl mx-auto sm:w-10/12 sm:h-full md:w-8/10 md:h-full lg:w-10/12 lg:h-full"> --}}
+      {{-- <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 bg-white border-b border-gray-200"> --}}
-          <canvas id="myChart"></canvas>
+
+{{-- <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:w-10/12 md:w-8/10 lg:w-10/12"> --}}
+      {{-- <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg"> --}}
+    <div class="relative m-auto sm:h-72" style="position: relative; height: 500px; width; 600px">
+        <canvas id="myChart"></canvas>
+    </div>
+          
             {{-- <a href="route('transaction.getdata')"> --}}
                 {{-- <button type="submit" id="getdata" onclick="getCharts()">chart</button> --}}
                 <button type="submit" id="getdata">chart</button>
 
             {{-- </a> --}}
-        {{-- </div>
-      </div>
+        {{-- </div> --}}
+      {{-- </div> --}}
     </div>
-  </div> --}}
+  </div>
 </x-app-layout>
 
