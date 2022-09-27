81f133e825353a4449ef94877a13534e7efa03c8
SHIO777 <shiotatsu1015@gmail.com>
2022-09-27 18:36:14 +0900

    [edit] route and dashboard

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,11 @@ use App\Http\Controllers\KindController;
 
 Route::group(['middleware' => 'auth'], function(){
     // 分析ページ
-    Route::post('/transaction/postdata', [TransactionController::class, 'postdata'])->name('transaction.postdata'); // post
-    Route::get('/transaction/getdata', [TransactionController::class, 'getdata'])->name('transaction.getdata'); // get
     Route::get('transaction/analyze', [TransactionController::class, 'mydata'])->name('transaction.analyze');   // analyze page UI
+    // 分析ページで非同期通信するときに使用
+    // Route::post('/transaction/postdata', [TransactionController::class, 'postdata'])->name('transaction.postdata'); // post
+    // Route::get('/transaction/getdata', [TransactionController::class, 'getdata'])->name('transaction.getdata'); // get
+    
     // 家計簿を入力するページ
     Route::resource('transaction', TransactionController::class);
     // 所得と支出のカテゴリーを追加するページ
